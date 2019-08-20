e829ac10d6a056fb7149ae82f65a64bbe3e9d8ae
Nimzyow <47536350+Nimzyow@users.noreply.github.com>
2019-08-20 22:20:48 +0100

    report part

---
+++ Components/Report/ReportP2.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Report.css";
+
+const ReportP2 = (props) => {
+    const borderStyle = {
+        //border: "1px solid black",
+        color: "black",
+        marginTop: "23%",
+        marginLeft: "5%",
+        fontFamily: "Arial",
+        fontSize: "12px"
+      };
+    return (
+        <div>
+        <div style={borderStyle}>
+            <p>TABLE OF CONTENTS</p>
+        </div>    
+        <div className="TableContents">
+            <div>
+            <p>1- AUTHORITY AND SCOPT OF STRUCTURAL INSPECTION FINDINGS</p>
+            <p>2- OBSERVATION & DESCRIPTION</p>
+            <p>3- FINDINGS</p>
+            <p>4- DISCUSSION & RECOMMENDATIONS</p>
+            <p>5- AN APPROXIMATE COST OF THE REMEDIAL WORK (STRUCTURAL)</p>
+            <p>6- APPENDIX A (Photos)</p>
+            </div>
+        </div>
+        </div>
+    )
+};
+
+export default ReportP2;
